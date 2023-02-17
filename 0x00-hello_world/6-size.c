0ffcf0299325f2af7a64a8a08f971d8d4a650490
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-16 22:51:24 -0800

    Edited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints exacty the size of datatype
- * charType: hold the size of char
- * intType: hold the size of intType
- * longIntType: hold the size of longInt
- * longLongIntType: hold the size of longLongInt
+ * @charType: hold the size of char
+ * @intType: hold the size of intType
+ * @longIntType: hold the size of longInt
+ * @longLongIntType: hold the size of longLongInt
  *
  * Return: 0 to terminate the program
  */
@@ -16,11 +16,13 @@ char charType;
 int intType;
 long int longIntType;
 long long int longLongIntType;
+float floatType;
 
 printf("Size of a char: %zu bytes\n", sizeof(charType));
 printf("Size of a int: %zu bytes\n", sizeof(intType));
 printf("Size of a long int: %zu bytes\n", sizeof(longIntType));
 printf("Size of a long long int: %zu byte\n", sizeof(longLongIntType));
+printf("Size of a float: %zu bytes\n", sizeof(floatType));
 
 return (0);
 
