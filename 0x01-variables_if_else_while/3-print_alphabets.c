21b09144084cd2919a35229af51b07b6c6b0a416
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-17 19:34:14 -0800

    Tasd3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,21 +9,21 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 	char ch1 = 'A';
 
-        while (ch <= 'z')
-        {
-                putchar(ch);
-                ch++;
-        }
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
 
 	while (ch1 <= 'Z')
-        {
-                putchar(ch1);
-                ch1++;
-        }
-        putchar('\n');
+	{
+		putchar(ch1);
+		ch1++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
