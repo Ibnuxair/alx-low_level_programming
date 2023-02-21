3a899ad66a5eca0a28a9bf5e4c17a7857b1e74c3
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-20 16:09:34 -0800

    Task100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * ch - hold the firt number
+ *
+ * Return: 0 to terminate the program
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
