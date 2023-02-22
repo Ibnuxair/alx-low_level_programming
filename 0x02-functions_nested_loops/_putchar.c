e7928022ffe48b2cf628ad93ff0bb029013afb6c
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-22 02:06:23 -0800

    Task0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <unistd.h>
 
-/* 
+/**
  * _putchar - prints the character c to the stdout
  * c - holds the character
  *
  *Return:  returns 0
  */
 
-int _putchar(int c)
+int _putchar(char c)
 {
         return (write(1, &c, 1));
 }
