52599471910a9b6c7e7bf5e0a2aa877b6da3edf7
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-23 03:52:20 -0800

    Task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
 #include "main.h"
 
 /**
- * positive_or_negative - prints whether number is +ve or -ve
- * @n: holds the number
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int n)
+int main(void)
 {
-	if (n > 0)
-	{
-		printf("%d", n);
-		printf(" is positive\n");
-	}
-	else if (n == 0)
-	{
-		printf("%d", n);
-		printf(" is zero\n");
-	}
-	else
-	{
-		printf("%d", n);
-		printf(" is negative\n");
-}
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
