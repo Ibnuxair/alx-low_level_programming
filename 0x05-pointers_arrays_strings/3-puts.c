43ab1da70f51df322c1a85a7bf719dfd87ba7cf2
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-02-27 19:12:20 -0800

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: is the pointer
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
