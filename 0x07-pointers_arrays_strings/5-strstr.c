90453f5166424a0d82c9919abbf2500e817c1468
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-03-06 18:38:02 -0800

    Task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,18 +13,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	size_t i;
 
-	if (*needle == '\0') {
-		return haystack;
+	if (*needle == '\0')
+	{
+		return (haystack);
 	}
 
 	for (i = 0; i < strlen(haystack); i++)
 	{
 		if (*(haystack + i) == *needle)
 		{
-			char* ptr = strstr(haystack + i + 1, needle + 1);
-			return (ptr) ? ptr - 1 : NULL;
+			char *ptr = strstr(haystack + i + 1, needle + 1);
+
+			return ((ptr) ? ptr - 1 : NULL);
 		}
 	}
 
-	return NULL;
+	return (NULL);
 }
