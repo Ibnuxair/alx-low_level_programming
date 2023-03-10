87d835ffee37e3bf673cfdade3ed167737d97c54
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-03-10 12:23:24 -0800

    Task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,11 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
