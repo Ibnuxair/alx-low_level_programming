da5af6f59ec6eab68c2bb31290326020c850a07e
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-03-17 08:58:38 -0700

    Task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,31 @@
-#include "main.h"
+ #include "main.h"
 
 /**
- * *_calloc - allocates memory for an array, using malloc
- * 
+ * *array_range - creates an array of integers
+ * @min: minimum input
+ * @max: maximum input
+ *
+ * Return: a pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int *a;
+
+	if (min > max)
+		return (NULL);
+
+	a = malloc((sizeof(int) * max - min) + sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+
+	while (min < max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+	return (a);
+}
