be9b680b4a7bb57daf2fa18f2c233823270e83ac
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-03-21 12:48:32 -0700

    Task0

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - prints the character c to the stdout
+ * c - holds the character
+ *
+ *Return:  returns 0
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
