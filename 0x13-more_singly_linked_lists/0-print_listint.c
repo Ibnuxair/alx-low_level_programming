9d24b75885f903a1abc3b28446a8bb310bb12f84
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-04-05 07:43:16 -0700

    Task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nNodes = 0;
 
-	if (h == NULL)
-		printf("nill\n");
-
 	while (h != NULL)
 	{
+		if (!h->n)
+			printf("nil\n");
+
 		printf("%d\n", h->n);
 		h = h->next;
 		nNodes++;
