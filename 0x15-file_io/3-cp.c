0cca0e77e23edf45145f2c731d8908be37bcd5d7
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-04-14 13:20:16 -0700

    Task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,71 +1,95 @@
 #include "main.h"
-
 /**
  * print_error_msg - prints the error message
  * @message: message to be printed
  */
-
-void print_error_msg(char* message)
+void print_error_msg(char *message)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", message);
+}
+
+/**
+ * close_fd - closes file descriptor
+ * @fd: file descriptor to be closed
+ */
+void close_fd(int fd)
+{
+	int x;
+
+	x = close(fd);
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * create_buff - allocates 1024 bytes for the buffer
+ * @file: file pointer to the file we're writing to
+ *
+ * Return: buffer pointer
+ */
+char *create_buff(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * BUFF_SIZE);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
 }
 
 /**
  * main - copies the content of a file to another file
  * @argc: arguments count
  * @argv[]: array of arguments
- *
  * Return: 0 always (success)
  */
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	char *src_file, *dest_file, buffer[BUFF_SIZE];
-	int fp_src, fp_dest;
+	char *buffer;
+	int fd_src, fd_dest;
 	ssize_t read_size, write_size;
 
-	if (argc != 3) {
-		print_error_msg("Usage: cp src_file dest_file\n");
+	if (argc != 3)
+	{
+		print_error_msg("Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	src_file = argv[1];
-	dest_file = argv[2];
+	buffer = create_buff(argv[2]);
+	fd_src = open(argv[1], O_RDONLY);
+	read_size = read(fd_src, buffer, BUFF_SIZE);
+	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-        fp_src = open(src_file, O_RDONLY);
-
-        fp_dest = open(dest_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	while ((read_size = read(fp_src, buffer, BUFF_SIZE)) > 0)
+	while (read_size > 0)
 	{
-		write_size = write(fp_dest, buffer, read_size);
-		if (write_size != read_size) {
-			print_error_msg(strerror(errno));
-			exit(-1);
+		if (fd_src == -1 || read_size == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
 		}
+
+		write_size = write(fd_dest, buffer, read_size);
+		if (fd_dest == -1 || write_size == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		read_size = read(fd_src, buffer, BUFF_SIZE);
 	}
 
-	if (fp_src == -1 || read_size == -1)
-	{
-		print_error_msg("Error: Can't read from src_file\n");
-		exit(98);
-	}
-
-	if (fp_dest == -1 || write_size == -1)
-        {
-                print_error_msg("Error: Can't write to dest_file\n");
-                exit(99);
-	}
-	if (close(fp_src) == -1)
-	{
-		print_error_msg("Error: Can't close fp fp_src\n");
-		exit(100);
-	}
-
-	if (close(fp_dest) == -1)
-	{
-		print_error_msg("Error: Can't close fp fp_dest\n");
-		exit(100);
-	}
+	free(buffer);
+	close(fd_src);
+	close(fd_dest);
 	return (0);
 }
