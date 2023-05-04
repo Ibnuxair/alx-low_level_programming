7458fb5c5d14aa6b9a4a7b677acc47936780224c
Ibrahim uzairu <ibrahimuzairu28@gmail.com>
2023-05-04 15:01:26 -0700

    Task6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int k;
+	char *pt = (char *) &k;
+
+	return (*pt);
+}
