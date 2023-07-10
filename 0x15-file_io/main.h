f343b42e4a6f638d30afc1c55bb2bca3e7daa901
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-07-10 04:06:43 -0700

    Task3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,12 +5,18 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
+/**macro**/
+#define BUFF_SIZE 1024
 
 /*prototypes*/
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error_msg(char *message);
+void close_fd(int fd);
+char *create_buff(char *file);
 
 #endif  /*ENDIF*/
