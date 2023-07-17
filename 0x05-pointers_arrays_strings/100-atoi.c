c31a73f58c753ccc5ca3b085a499c879f1ab30ff
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-07-17 04:53:40 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,14 +11,21 @@ int _atoi(char *s)
 {
 	int res = 0;
 	int sign = 1;
-	int is_digit = 0;
+	int digit, is_digit = 0;
 
 	while (*s != '\0')
 	{
 		if (*s >= '0' && *s <= '9')
 		{
 			is_digit = 1;
-			res = res * 10 + (*s - '0');
+			digit = *s - '0';
+			if (res > INT_MAX / 10 ||
+			    (res == INT_MAX / 10 && digit > INT_MAX % 10))
+			{
+				/* Integer overflow occurred */
+				return ((sign == 1) ? INT_MAX : INT_MIN);
+			}
+			res = res * 10 + digit;
 		}
 		else if (is_digit)
 		{
@@ -31,5 +38,5 @@ int _atoi(char *s)
 		s++;
 	}
 
-	return res * sign;
+	return (res * sign);
 }
