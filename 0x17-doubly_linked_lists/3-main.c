8ddefbfe395bae35893c872375a06e32411cbc1c
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-17 09:39:01 -0700

    Task3

---
+++ 0x17-doubly_linked_lists/3-main.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: None.
+ */
+void free_dlistint_t(dlistint_t *head)
+{
+        dlistint_t *curr;
+
+        while (head != NULL)
+        {
+                curr = head;
+                head = head->next;
+                free(curr);
+        }
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    free_dlistint_t(head);
+    return (EXIT_SUCCESS);
+}
