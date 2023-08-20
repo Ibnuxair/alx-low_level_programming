199f606f5d6a68a5ad00089bd74c1110c768b332
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-20 06:49:28 -0700

    Task7

--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -4,42 +4,24 @@
 #include "lists.h"
 
 /**
- * free_dlistint - Frees a dlistint_t list.
- * @head: Pointer to the head of the list.
- *
- * Return: None.
- */
-void free_dlistint_t(dlistint_t *head)
-{
-	dlistint_t *curr;
-
-	while (head != NULL)
-	{
-		curr = head;
-		head = head->next;
-		free(curr);
-	}
-}
-
-/**
  * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    dlistint_t *head;
+	dlistint_t *head;
 
-    head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
-    print_dlistint(head);
-    free_dlistint_t(head);
-    return (EXIT_SUCCESS);
+	head = NULL;
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	add_dnodeint(&head, 4);
+	add_dnodeint(&head, 98);
+	add_dnodeint(&head, 402);
+	add_dnodeint(&head, 1024);
+	print_dlistint(head);
+	free_dlistint_t(head);
+	return (EXIT_SUCCESS);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,9 +27,9 @@ unsigned int dlist_len(dlistint_t *h)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *curr, *new_node, *prev_node;
+	dlistint_t *curr, *new_node;
 	unsigned int len = dlist_len(*h);
-	unsigned int i;
+	unsigned int i = 1;
 
 	if (idx > len)
 		return (NULL);
@@ -39,17 +39,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	if (idx == len)
 		return (add_dnodeint_end(h, n));
+
 	curr = *h;
-	prev_node = NULL;
 	/* Traverse to the node before the desired index */
-	for (i = 0; i < idx && curr != NULL; i++)
+	while (i < idx)
 	{
-		prev_node = curr;
 		curr = curr->next;
+		i++;
 	}
-	/* Check if the desired index was reached */
-	if (i != idx)
-		return (NULL);
 
 	/* Create the new node and update its pointers */
 	new_node = malloc(sizeof(dlistint_t));
@@ -57,14 +54,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = prev_node;
-	new_node->next = curr;
-
-	if (prev_node != NULL)
-		prev_node->next = new_node;
-
-	if (curr != NULL)
-		curr->prev = new_node;
+	new_node->prev = curr;
+	new_node->next = curr->next;
+	curr->next = new_node;
+	new_node->next->prev = new_node;
 
 	return (new_node);
 }
