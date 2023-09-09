21496b62e8d0b767476063f4417783ec10eae95d
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-09-09 11:32:50 -0700

    Task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,14 +46,25 @@ hash_node_t *create_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *curr_node;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = create_node(key, value);
+	curr_node = ht->array[index];
+	while (curr_node != NULL)
+	{
+		if (strcmp(curr_node->key, key) == 0)
+		{
+			free(curr_node->value);
+			curr_node->value = strdup(value);
+			return (1);
+		}
+		curr_node = curr_node->next;
+	}
 
+	new_node = create_node(key, value);
 	if (!new_node)
 		return (0);
 
