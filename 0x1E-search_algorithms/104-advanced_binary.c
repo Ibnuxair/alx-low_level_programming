6a1c265aece656bc5c45f9ec6579b4a7bcfdaa01
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-12-13 06:17:01 -0800

    Task11

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -21,20 +21,21 @@ int advanced_binary(int *array, size_t size, int value)
 		printf("%d, ", array[i]);
 	printf("%d\n", array[i]);
 
-	mid = size / 2;
+	mid = (size - 1) / 2;
+
 	if (array[mid] == value)
 	{
-		if (size == 1 || array[mid - 1] < value)
+		if (mid == 0 || array[mid - 1] < value)
 			return (mid);
 	}
 
 	if (array[mid] < value)
 	{
 		found = advanced_binary(array + mid + 1, size - mid - 1, value);
-		return (found != -1 ? (int)(mid + found + 1) : -1);
+		return (found != -1 ? (int)(mid + 1 + found) : -1);
 	}
 	else
 	{
-		return (advanced_binary(array, mid, value));
+		return (advanced_binary(array, mid + 1, value));
 	}
 }
